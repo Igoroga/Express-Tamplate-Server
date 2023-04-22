5876c1efaac9a15f15ce13ffb0ba4bd048e06fd2
Igor Gusakov <igorgusakov199918@gmail.com>
2023-04-22 19:43:34 +0600

    version3

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 import express from "express";
 import mongoose from 'mongoose';
+import router from "./router.js";
 
 const PORT = 5000;
 const DB_URL = "mongodb+srv://igorgusakov199918:hWtLMmZVreA0cyXE@cluster0.u7wpmpi.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
 
 app.use(express.json())
-
-app.get('/user', (req, res) => {
-console.log(req.body);
-res.status(200).json( "сервер работает123");
-});
+app.use('/api', router)
 
 
 async function startApp() {
     try {
         await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
         console.log("Connected to MongoDB!");
+        const db = mongoose.connection.db;
+        const users = db.collection('users');
+        await users.insertOne({name: 'ulbi tv', age: 21})
+        const count = await users.countDocuments()
+        const user = await users.findOne({name: 'ulbi tv'})
+        console.log(count); 
         app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
     } catch (e) {
         console.log(e)
